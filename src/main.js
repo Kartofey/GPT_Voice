1c442a6693e34ccf6d91287c039c6b53e0f70a29
Kartofey <potato316bless@gmail.com>
2023-05-25 01:06:41 +0400

    Add_response_from_gpt_by_text_message

--- src/main.js
+++ src/main.js
@@ -1,19 +1,25 @@
 import { Telegraf } from 'telegraf'
 import { message } from 'telegraf/filters';
+import { code } from 'telegraf/format';
 import config from 'config';
 import { ogg } from './ogg.js';
+import { openai } from './openai.js'
 
 const bot = new Telegraf(config.get('TELEGRAM_TOKEN'))
 
 bot.on(message('voice'), async ctx => {
   try {
+    await ctx.reply(code('Ожидание ответа...'))
     const link = await ctx.telegram.getFileLink(ctx.message.voice.file_id)
     const userID = String(ctx.message.from.id)
-    console.log(link.href)
     const oggPath = await ogg.create(link.href, userID)
     const mp3Path = await ogg.toMp3(oggPath, userID)
-    await ctx.reply(mp3Path)
-    await ctx.reply(JSON.stringify(link, null, 2))
+
+    const text = await openai.transcription(mp3Path)
+    const messages = [{role: openai.roles.USER, content: text}]
+    // const response = await openai.chat(message)
+
+    await ctx.reply(text)
   } catch (e) {
     console.log(`Error while voice message`, e.message)
   }
